0dbfe85e9fc0d158e1e7b60cf3f0b51d36add3f9
ArcanusNEO <lucasding@foxmail.com>
2022-02-27 23:30:27 +0800

    .

--- 1/0/src/main.cc
+++ 1/0/src/main.cc
@@ -22,14 +22,24 @@ signed main() {
 
   for (int i = 1; i <= vec.size(); ++i) cin >> vec(i);
 
+  auto t1 = chrono::steady_clock::now();
   // 逐列访问的平凡算法
   for (int i = 1; i <= matrix.col(); ++i)
     for (int j = 1; j <= matrix.row(); ++j)
       ans1 += (long long) matrix(j, i) * vec(i);
+  cout << "逐列访问的平凡算法" << endl;
+  cout << "结果：" << ans1 << endl;
+  auto t2  = chrono::steady_clock::now();
+  auto sec = chrono::duration_cast<chrono::duration<double>>(t2 - t1);
+  cout << sec.count() << " s" << endl;
   // cache优化算法
   for (int i = 1; i <= matrix.row(); ++i)
     for (int j = 1; j <= matrix.col(); ++j)
       ans2 += (long long) matrix(i, j) * vec(j);
+  auto t3 = chrono::steady_clock::now();
 
-  cout << ans1 << " " << ans2 << endl;
+  cout << "cache优化算法" << endl;
+  cout << "结果：" << ans2 << endl;
+  sec = chrono::duration_cast<chrono::duration<double>>(t3 - t2);
+  cout << sec.count() << " s" << endl;
 }
