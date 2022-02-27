65568017797650309706ba83c1028f9a6c657b7c
ArcanusNEO <lucasding@foxmail.com>
2022-02-27 20:25:48 +0800

    .

--- 1/0/inc/matrix.hh
+++ 1/0/inc/matrix.hh
@@ -8,14 +8,7 @@
 
 template <typename T> class matrix_t {
 public:
-  size_t n, m;
-  // n 行 m 列
-
-  std::unique_ptr<T> arr;
-  matrix_t() {
-    n = m = 0;
-    arr.release();
-  }
+  matrix_t() { n = m = 0; }
   matrix_t(const matrix_t& other) {
     n = other.n;
     m = other.m;
@@ -28,6 +21,12 @@ public:
     arr.release();
     arr.swap(other.arr);
   }
+
   T& operator()(size_t x, size_t y) { return arr[(x - 1) * m - 1 + y]; }
   T  operator()(size_t x, size_t y) const { return arr[(x - 1) * m - 1 + y]; }
+
+protected:
+  size_t n, m;
+  // n 行 m 列
+  std::unique_ptr<T> arr;
 };
--- 1/0/src/main.cc
+++ 1/0/src/main.cc
@@ -1,5 +1,8 @@
 #include <bits/stdc++.h>
+
+#include "matrix.hh"
 using namespace std;
 signed main() {
+  matrix_t<double> m;
   cout << "Hello, world!" << endl;
 }
