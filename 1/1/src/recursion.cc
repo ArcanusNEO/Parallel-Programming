8de314c88dfcbae4d1ae1ac51334ac16efd75f1a
ArcanusNEO <lucasding@foxmail.com>
2022-03-06 11:40:11 +0800

    .

--- 1/1/src/recursion.cc
+++ 1/1/src/recursion.cc
@@ -2,9 +2,16 @@
 #include "solve.hh"
 using namespace std;
 
-void func(int& ans, const int arr[], int n) {
-  for (int i = 0; i < n; ++i) ans += arr[i];
+int recur(const int arr[], int n) {
+  if (n <= 1) return *arr;
+  int halfn = n >> 1;
+  return recur(arr, halfn) + recur(arr + halfn, halfn);
 }
+
+void func(int& ans, const int arr[], int n) {
+  ans = recur(arr, n);
+}
+
 signed main(int argc, char* argv[]) {
   return _main(argc, argv);
 }
