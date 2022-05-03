4900a8e254c720d652e0fdf2c839a661936f3b8b
ArcanusNEO <lucasding@foxmail.com>
2022-05-03 17:19:11 +0800

    .

--- 4/src/static-sem-all.cc
+++ 4/src/static-sem-all.cc
@@ -15,9 +15,11 @@ struct thread_param_t {
   int t_id;
 };
 
-sem_t sem_leader;
-sem_t sem_div[MAX_SUB_THREAD];
-sem_t sem_elim[MAX_SUB_THREAD];
+sem_t          sem_leader;
+sem_t          sem_div[MAX_SUB_THREAD - 1];
+sem_t          sem_elim[MAX_SUB_THREAD - 1];
+pthread_t      handle[MAX_SUB_THREAD];
+thread_param_t param[MAX_SUB_THREAD];
 
 void* thread_func(void* param) {
   auto p    = (thread_param_t*) param;
@@ -28,32 +30,42 @@ void* thread_func(void* param) {
         matrix(k, j) = matrix(k, j) / matrix(k, k);
       matrix(k, k) = 1.0;
     } else sem_wait(sem_div + t_id - 1);
+    if (t_id == 0)
+      for (int i = 0; i < MAX_SUB_THREAD - 1; ++i) sem_post(sem_div + i);
+    for (int i = k + 1 + t_id; i < n; i += MAX_SUB_THREAD) {
+      for (int j = k + 1; j < n; ++j)
+        matrix(i, j) = matrix(i, j) - matrix(i, k) * matrix(k, j);
+      matrix(i, k) = 0.0;
+    }
+    if (t_id == 0) {
+      for (int i = 0; i < MAX_SUB_THREAD - 1; ++i) sem_wait(&sem_leader);
+      for (int i = 0; i < MAX_SUB_THREAD - 1; ++i) sem_post(sem_elim + i);
+    } else {
+      sem_post(&sem_leader);
+      sem_wait(sem_elim + t_id - 1);
+    }
   }
-  for (int j = k + 1; j < n; ++j)
-    matrix(i, j) = matrix(i, j) - matrix(i, k) * matrix(k, j);
-  matrix(i, k) = 0;
   pthread_exit(nullptr);
 }
 
 void func(int& ans, float arr[], int n) {
   ::n   = n;
   ::arr = arr;
-  for (int k = 0; k < n; ++k) {
-    for (int j = k + 1; j < n; ++j) matrix(k, j) = matrix(k, j) / matrix(k, k);
-    matrix(k, k)     = 1.0;
-    int worker_count = n - 1 - k;
-    for (int offset = 0; offset < worker_count; offset += MAX_SUB_THREAD) {
-      for (int t_id = 0, i = t_id + offset;
-           i < worker_count && t_id < MAX_SUB_THREAD;
-           ++t_id, i = t_id + offset) {
-        thread_param[t_id] = {k, i};
-        pthread_create(thread_handle + t_id, nullptr, thread_func,
-                       thread_param + t_id);
-      }
-      for (int t_id = 0, i                                      = t_id + offset;
-           i < worker_count && t_id < MAX_SUB_THREAD; ++t_id, i = t_id + offset)
-        pthread_join(thread_handle[t_id], nullptr);
-    }
+  sem_init(&sem_leader, 0, 0);
+  for (int i = 0; i < MAX_SUB_THREAD - 1; ++i) {
+    sem_init(sem_div + i, 0, 0);
+    sem_init(sem_elim + i, 0, 0);
+  }
+  for (int t_id = 0; t_id < MAX_SUB_THREAD; ++t_id) {
+    param[t_id].t_id = t_id;
+    pthread_create(handle + t_id, nullptr, thread_func, param + t_id);
+  }
+  for (int t_id = 0; t_id < MAX_SUB_THREAD; ++t_id)
+    pthread_join(handle[t_id], nullptr);
+  sem_destroy(&sem_leader);
+  for (int i = 0; i < MAX_SUB_THREAD - 1; ++i) {
+    sem_destroy(sem_div + i);
+    sem_destroy(sem_elim + i);
   }
 }
 #undef matrix
