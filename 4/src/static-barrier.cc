f071baa3ec76a4b218671a9b0bc2914d15f4fb9e
ArcanusNEO <lucasding@foxmail.com>
2022-05-04 19:34:22 +0800

    .

---
+++ 4/src/static-barrier.cc
@@ -0,0 +1,66 @@
+#include <pthread.h>
+#include <semaphore.h>
+
+#include "main.hh"
+#include "solve.hh"
+using namespace std;
+#define matrix(i, j) arr[(i) *n + (j)]
+
+#define MAX_SUB_THREAD 7
+
+int    n;
+float* arr;
+
+struct thread_param_t {
+  int t_id;
+};
+
+pthread_barrier_t barrier_div;
+pthread_barrier_t barrier_elim;
+pthread_t         handle[MAX_SUB_THREAD];
+thread_param_t    param[MAX_SUB_THREAD];
+
+void* thread_func(void* param) {
+  auto p    = (thread_param_t*) param;
+  auto t_id = p->t_id;
+  for (int k = 0; k < n; ++k) {
+    if (t_id == 0) {
+      for (int j = k + 1; j < n; ++j)
+        matrix(k, j) = matrix(k, j) / matrix(k, k);
+      matrix(k, k) = 1.0;
+    }
+
+    pthread_barrier_wait(&barrier_div);
+
+    for (int i = k + 1 + t_id; i < n; i += MAX_SUB_THREAD) {
+      for (int j = k + 1; j < n; ++j)
+        matrix(i, j) = matrix(i, j) - matrix(i, k) * matrix(k, j);
+      matrix(i, k) = 0.0;
+    }
+
+    pthread_barrier_wait(&barrier_elim);
+  }
+  pthread_exit(nullptr);
+}
+
+void func(int& ans, float arr[], int n) {
+  ::n   = n;
+  ::arr = arr;
+  pthread_barrier_init(&barrier_div, nullptr, MAX_SUB_THREAD);
+  pthread_barrier_init(&barrier_elim, nullptr, MAX_SUB_THREAD);
+
+  for (int t_id = 0; t_id < MAX_SUB_THREAD; ++t_id) {
+    param[t_id].t_id = t_id;
+    pthread_create(handle + t_id, nullptr, thread_func, param + t_id);
+  }
+  for (int t_id = 0; t_id < MAX_SUB_THREAD; ++t_id)
+    pthread_join(handle[t_id], nullptr);
+
+  pthread_barrier_destroy(&barrier_div);
+  pthread_barrier_destroy(&barrier_elim);
+}
+#undef matrix
+
+signed main(int argc, char* argv[]) {
+  return _main(argc, argv);
+}
