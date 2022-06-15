b1b7cd85e092801d5804e8761c38be54d551aa58
ArcanusNEO <lucasding@foxmail.com>
2022-06-15 18:17:26 +0800

    .

--- 6/src/cuda.cu
+++ 6/src/cuda.cu
@@ -1,20 +1,51 @@
+#include <cuda_runtime.h>
+#include <device_launch_parameters.h>
+
 #include "main.hh"
 #include "solve.hh"
 using namespace std;
 
-void func(int& ans, float arr[], int n) {
 #define matrix(i, j) arr[(i) *n + (j)]
-  for (int k = 0; k < n; ++k) {
-    for (int j = k + 1; j < n; ++j) matrix(k, j) = matrix(k, j) / matrix(k, k);
-    matrix(k, k) = 1.0;
-    for (int i = k + 1; i < n; ++i) {
-      for (int j = k + 1; j < n; ++j)
-        matrix(i, j) = matrix(i, j) - matrix(i, k) * matrix(k, j);
-      matrix(i, k) = 0;
-    }
-  }
-#undef matrix
+
+__global__ void division_kernel(float arr[], int n, int k) {
+  auto tid = blockDim.x * blockIdx.x + threadIdx.x;
+  if (k < tid && tid < n) matrix(k, tid) /= matrix(k, k);
 }
+
+__global__ void eliminate_kernel(float arr[], int n, int k) {
+  auto tid = blockDim.x * blockIdx.x + threadIdx.x;
+  if (tid == 0) matrix(k, k) = 1.0;
+}
+
+void func(int& ans, float arr[], int n) {
+  float* gpu_arr;
+  auto   siz = sizeof(float) * n * n;
+  if (cudaMalloc(&gpu_arr, siz) != cudaSuccess)
+    cerr << "cudaMalloc failed" << endl;
+  if (cudaMemcpy(gpu_arr, arr, siz, cudaMemcpyHostToDevice) != cudaSuccess)
+    cerr << "cudaMemcpyHostToDevice failed" << endl;
+
+  for (int k = 0; k < n; ++k) {
+    auto rest = n - k;
+    dim3 grid(std::ceil(rest / 1024.0));
+    dim3 block(1024);
+    division_kernel<<<grid, block>>>(gpu_arr, n, k);
+    cudaDeviceSynchronize();
+    if (auto ret = cudaGetLastError(); ret != cudaSuccess)
+      cerr << "division kernel failed: " << cudaGetErrorString(ret) << endl;
+    matrix(k, k) = 1.0;
+    eliminate_kernel<<<grid, block>>>(gpu_arr, n, k);
+    cudaDeviceSynchronize();
+    if (auto ret = cudaGetLastError(); ret != cudaSuccess)
+      cerr << "eliminate kernel failed: " << cudaGetErrorString(ret) << endl;
+  }
+  if (cudaMemcpy(arr, gpu_arr, siz, cudaMemcpyDeviceToHost) != cudaSuccess)
+    cerr << "cudaMemcpyDeviceToHost failed" << endl;
+  cudaFree(gpu_arr);
+}
+
+#undef matrix
+
 signed main(int argc, char* argv[]) {
   return _main(argc, argv);
 }
