c0fa65adcf4e9c588986ee5d62805450d1151fc0
ArcanusNEO <lucasding@foxmail.com>
2022-06-23 00:02:09 +0800

    .

--- 7/src/mpi-slow.cc
+++ 7/src/mpi-slow.cc
@@ -26,11 +26,21 @@ void mpi_sub_func(float arr[], int n) {
     }
     int bc_rank = min(k / block_sz, comm_sz - 1);
     MPI_Bcast(prow(k), n, MPI_FLOAT, bc_rank, MPI_COMM_WORLD);
-    for (int i = row_begin; i < row_end; ++i) {
-      if (i <= k) continue;
+    int inner_block_sz  = (n - k - 1) / comm_sz;
+    int inner_row_begin = k + 1 + inner_block_sz * my_rank;
+    int inner_row_end =
+      (my_rank + 1 == comm_sz ? n : inner_row_begin + inner_block_sz);
+    for (int i = inner_row_begin; i < inner_row_end; ++i) {
       for (int j = k + 1; j < n; ++j)
         matrix(i, j) -= matrix(i, k) * matrix(k, j);
       matrix(i, k) = 0;
+    }
+    for (int i = 0; i < comm_sz; ++i) {
+      int local_row_begin = k + 1 + inner_block_sz * i;
+      int local_row_end =
+        (i + 1 == comm_sz ? n : local_row_begin + inner_block_sz);
+      MPI_Bcast(prow(local_row_begin), (local_row_end - local_row_begin) * n,
+                MPI_FLOAT, i, MPI_COMM_WORLD);
     }
   }
   // MPI_Barrier(MPI_COMM_WORLD);
