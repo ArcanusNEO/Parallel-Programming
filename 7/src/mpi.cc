fa60d7c9b176c8cb7df99a032adb0a786b7dfd33
ArcanusNEO <lucasding@foxmail.com>
2022-06-22 23:55:27 +0800

    .

--- 7/src/mpi.cc
+++ 7/src/mpi.cc
@@ -24,7 +24,8 @@ void mpi_sub_func(float arr[], int n) {
       for (int j = k + 1; j < n; ++j) matrix(k, j) /= matrix(k, k);
       matrix(k, k) = 1.0;
     }
-    MPI_Bcast(prow(k), n, MPI_FLOAT, my_rank, MPI_COMM_WORLD);
+    int bc_rank = min(k / block_sz, comm_sz - 1);
+    MPI_Bcast(prow(k), n, MPI_FLOAT, bc_rank, MPI_COMM_WORLD);
     int inner_block_sz  = (n - k - 1) / comm_sz;
     int inner_row_begin = k + 1 + inner_block_sz * my_rank;
     int inner_row_end =
@@ -34,8 +35,13 @@ void mpi_sub_func(float arr[], int n) {
         matrix(i, j) -= matrix(i, k) * matrix(k, j);
       matrix(i, k) = 0;
     }
-    MPI_Bcast(prow(inner_row_begin), (inner_row_end - inner_row_begin) * n,
-              MPI_FLOAT, my_rank, MPI_COMM_WORLD);
+    for (int i = 0; i < comm_sz; ++i) {
+      int local_row_begin = k + 1 + inner_block_sz * i;
+      int local_row_end =
+        (i + 1 == comm_sz ? n : local_row_begin + inner_block_sz);
+      MPI_Bcast(prow(local_row_begin), (local_row_end - local_row_begin) * n,
+                MPI_FLOAT, i, MPI_COMM_WORLD);
+    }
   }
   // MPI_Barrier(MPI_COMM_WORLD);
 }
